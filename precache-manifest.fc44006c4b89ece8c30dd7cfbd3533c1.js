fce6219426cec2823afaac91730ab18725a8285c
Sid Hackney <snackney@gmail.com>
2020-06-14 16:30:16 -0400

    Updates

---
+++ precache-manifest.fc44006c4b89ece8c30dd7cfbd3533c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cf12b7c6aa933f380692dd1b24a6e2",
+    "url": "/steel-defender/index.html"
+  },
+  {
+    "revision": "c0eef333baa1700b4701",
+    "url": "/steel-defender/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "430c9edc888f9a6fc58a",
+    "url": "/steel-defender/static/js/2.fcfb5b0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/steel-defender/static/js/2.fcfb5b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0eef333baa1700b4701",
+    "url": "/steel-defender/static/js/main.b46c45b9.chunk.js"
+  },
+  {
+    "revision": "78649e7abaa25239fa64",
+    "url": "/steel-defender/static/js/runtime-main.3c0ca0ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/steel-defender/static/media/logo.5d5d9eef.svg"
+  }
+]);
